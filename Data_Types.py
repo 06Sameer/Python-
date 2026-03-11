fa50c283967c6468ac5bcfb77226a94156a01cee
06Sameer <pandeysameer637@gmail.com>
2026-03-11 11:05:57 +0530

    Add Data_Types.py to demonstrate variable types

---
+++ Data_Types.py
@@ -0,0 +1,10 @@
+a = 90
+b = "Sameer"
+c = 17.77
+d = 1+2j
+
+
+print(type(a))  ##<class 'int'> 
+print(type(b))  ##<class 'str'> 
+print(type(c))  ##<class 'float'> 
+print(type(d))  ##<class 'complex'> 
